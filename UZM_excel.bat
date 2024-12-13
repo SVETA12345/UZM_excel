412bac2117a89d47e923d48dc7d37a15ac5cf241
Svetlana Martyanova <smartianova8@mail.ru>
2024-12-13 15:35:32 +0300

    update bat file

--- UZM_excel.bat
+++ UZM_excel.bat
@@ -1,1 +1,6 @@
-cmd /c python manage.py runserver 10.23.125.145:9680
+mkdir files\Report_input
+mkdir files\Report_out
+python -m venv venv
+call venv\Scripts\activate.bat
+pip install -r requirements.txt
+python manage.py runserver 10.23.125.145:9680
